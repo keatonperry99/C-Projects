8525f1b6e9fd8dd3288486fb8020b6f12c4eb9a7
Keaton Perry <54960503+keatonperry99@users.noreply.github.com>
2020-02-04 10:42:11 -0800

    Update rooms.c

--- rooms.c
+++ rooms.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-// The struct for the rooms
+/*
+The struct for the rooms
+*/
 typedef struct {
 	char *RoomName;
 	char *RoomType;
@@ -11,7 +13,71 @@ typedef struct {
 	char** Connections;
 }Room;
 
-// Function to set the names of the rooms
+/*
+Function prototypes
+*/
+void setNames(Room* r);
+
+void setTypes(Room* r);
+
+int isGraphFull(Room* r);
+
+Room* getRandomRoom(Room* r) {
+	int idx = rand() % 7;
+	return &(r[idx]);
+}
+
+/*
+Main function
+*/
+void main() {
+	/*
+	The command to enable random numbers
+	*/
+	srand(time(NULL));
+
+	/*
+	Declaring the array of structs
+	*/
+	Room* r = malloc(7 * sizeof(Room));
+	int i;
+	for (i = 0; i < 6; i++) {
+		r[i].Connections = malloc(6 * sizeof(char*));
+	}
+
+	/*
+	Setting the names of the rooms
+	*/
+	setNames(r);
+
+	/*
+	Setting the names of the rooms
+	*/
+	setTypes(r);
+
+	/*
+	Setting the initial number of connections for each room
+	*/
+	for (i = 0; i < 10; i++) {
+		r[i].NumConnections = 0;
+	}
+	
+	/*
+	Creating all the connections
+	*/
+	Room* A;
+	A = getRandomRoom(r);
+	A->RoomName = "TESTING";
+	
+	for (int i = 0; i < 7; i++) {
+		printf("%s, ", r[i].RoomName);
+	}
+
+}
+
+/*
+Function to set the names of the rooms
+*/
 void setNames(Room* r) {
 	char* RoomNames[10];
 	RoomNames[0] = "Train";
@@ -25,62 +91,51 @@ void setNames(Room* r) {
 	RoomNames[8] = "Stones";
 	RoomNames[9] = "Dark";
 
-	// Getting 7 random numbers between 0 and 9
+	/*
+	Getting 7 random numbers between 0 and 9
+	*/
 	int nums[10];
-	for (int i = 0; i < 10; i++) {
+	int i;
+	for (i = 0; i < 10; i++) {
 		nums[i] = i;
 	}
-	for (int i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++) {
 		int tmp = nums[i];
 		int idx = rand() % 7;
 		nums[i] = nums[idx];
 		nums[idx] = tmp;
 	}
 
-	// Setting the room names
-	for (int i = 0; i < 7; i++) {
+	/*
+	Setting the room names
+	*/
+	for (i = 0; i < 7; i++) {
 		r[i].RoomName = RoomNames[nums[i]];
 	}
 }
 
-// Function to assign room types to all the rooms
+/*
+Function to assign room types to all the rooms
+*/
 void setTypes(Room* r) {
 	r[0].RoomType = "START_ROOM";
 	r[6].RoomType = "END_ROOM";
-	for (int i = 1; i < 6; i++) {
+	int i;
+	for (i = 1; i < 6; i++) {
 		r[i].RoomType = "MID_ROOM";
 	}
 }
 
-
-// Main function
-void main() {
-	// The command to enable random numbers
-	srand(time(NULL));
-
-	// Declaring the array of structs
-	Room* r = malloc(7 * sizeof(Room));
-	for (int i = 0; i < 6; i++) {
-		r[i].Connections = malloc(6 * sizeof(char*));
+/*
+Function to check to see if the graph is full. Returns 1 if
+it's full, 0 if it's not.
+*/
+int isGraphFull(Room* r) {
+	int i;
+	for (i = 0; i < 7; i++) {
+		if (r[i].NumConnections < 3) {
+			return 0;
+		}
 	}
-
-	// Setting the names of the rooms
-	setNames(r);
-
-	// Setting the room types of the rooms
-	setTypes(r);
-
-	// Setting the initial number of connections for each room
-	for (int i = 0; i < 10; i++) {
-		r[i].NumConnections = 0;
-	}
-	
-	printf("Room Names: ");
-	for (int i = 0; i < 7; i++) {
-		printf("%s, ", r[i].RoomName);
-	}
-	printf("\nRoom Types: ");
-	for (int i = 0; i < 7; i++) {
-		printf("%s, ", r[i].RoomType);
-	}
+	return 1;
 }
