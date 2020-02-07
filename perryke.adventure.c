9a9583ef897faa28c817360b691c65b45c5e5867
Keaton Perry <54960503+keatonperry99@users.noreply.github.com>
2020-02-06 17:22:37 -0800

    Update perryke.adventure.c

--- perryke.adventure.c
+++ perryke.adventure.c
@@ -3,42 +3,35 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <unisted.h>
 
-void main()
-{
-	int newestDirTime = -1; // The modified timestamp of the newest subdir we've examined
-	char targetDirPrefix[32] = "brewsteb.rooms."; // The prefix we're looking for for all subdirs
-	char newestDirName[256]; // Holds the name of the newest directory that contains the prefix
+void main() {
+	int newestDirTime = -1;
+	char targetDirPrefix[32];
+	char newestDirName[256];
 	memset(newestDirName, '\0', sizeof(newestDirName));
 
-	DIR* dirToCheck; // Holds the directory we're starting in
-	struct dirent *fileInDir; // Holds the current subdir of the starting dir we're looking at
-	struct stat dirAttributes; // Holds information we've gained about subdir
-	
-	dirToCheck = opendir("."); // Open up the directory this program was run in
+	DIR* dirToCheck;
+	struct dirent* fileInDir;
+	struct stat dirAttributes;
 
-	if (dirToCheck > 0) // Make sure the current directory could be opened
-	{ 
-		while ((fileInDir = readdir(dirToCheck)) != NULL) // Check each entry in the directory
-		{
-			if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) // If entry has the prefix
-			{
-				printf("Found the prefex: %s\n", fileInDir->d_name);
-		                stat(fileInDir->d_name, &dirAttributes); // Get attributes of the entry
-				
-				if ((int)dirAttributes.st_mtime > newestDirTime) // If this time is bigger
-				{
+	dirToCheck = opendir(".");
+
+	if (dirToCheck > 0) {
+		while ((fileInDir = readdir(dirToCheck)) != NULL) {
+			if (strstr(fileInDir->d_name, targetDirPrefix) != NULL) {
+				stat(fileInDir->d_name, &dirAttributes);
+				if ((int)dirAttributes.st_mtime > newestDirTime) {
 					newestDirTime = (int)dirAttributes.st_mtime;
 					memset(newestDirName, '\0', sizeof(newestDirName));
 					strcpy(newestDirName, fileInDir->d_name);
 					printf("Newer subdir: %s, new time: %d\n", fileInDir->d_name, newestDirTime);
 				}
 			}
-		} 
-	}   		
+		}
+	}
 
-	closedir(dirToCheck); // Close the directory we opened
+	closedir(dirToCheck);
 
-	printf("Newest entry found is: %s\n", newestDirName);
+	printf("Name of the directory is: %s\n", newestDirName);
 }
